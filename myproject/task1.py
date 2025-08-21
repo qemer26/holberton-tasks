078268daaa00b15e1a565de19b98dfb69424f123
qemer26 <qemerqehremanova817@gmail.com>
2025-08-21 13:08:56 +0400

    task

---
+++ myproject/task1.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def style():
+    return'''
+    <h1 style="color: red;">Salam Taylor Swift!</h1>
+    <h2 style="color: black;">Look what you made me do!</h1>
+    <h3 style="color: blue;">Bad blood!</h3>
+     <p style="color: orange; ">Showgirl.</p>
+     <p style="color: pink; ">Enchanted.</p>
+     '''
+if __name__=='__main__':
+    app.run(debug=True, port=5007)
